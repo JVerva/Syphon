797c6ee7eccd7618a3b003bd89fa932a952b2a85
Joao Vera <joaoguedessoaresvera@gmail.com>
2021-12-16 15:15:04 +0000

    first commit

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float walkSpeed=1;
+    public float runSpeed = 1;
+    public float jumpHeight = 1;
+}
